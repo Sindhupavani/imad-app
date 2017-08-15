cb0fb44067a664e746ccae67d983d2fe3d57876b
Sindhupavani <30889721+Sindhupavani@users.noreply.github.com>
2017-08-15 11:34:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/', function (req, res) {
+app.get('/pavani.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'pavani.html'));
 });
 
